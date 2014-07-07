9e90348956e0fb8313955bc1f260e3d1ee7dc09d
Cliff Flood <cliph@cli.ph>
2014-07-07 18:04:01 -0400

    further updates, specify options, image size, add a clean function and enable numbered looping

--- camsnap.sh
+++ camsnap.sh
@@ -3,9 +3,10 @@
 imgsnap=~/bin/ImageSnap-v0.2.5/imagesnap
 dropbox=~/Dropbox
 rpisize="-w 1280 -h 720"
-rpisnap="/usr/bin/raspistill ${rpisize} -vf -hf -o "
+rpiopts="-awb auto -ex auto -q 15"
+rpisnap="/usr/bin/raspistill ${rpisize} ${rpiopts} -vf -hf -o "
 rpipath=/home/cliph/www/cam
-interval=5
+interval=45
 
 if [ -x $imgsnap ]
 then
@@ -17,6 +18,34 @@ then
 else
    exit 0
 fi
+
+function clean {
+   echo -n "Cleaning $outpath ... "
+   cd $outpath
+   shopt -s nullglob
+   files=( *.jpg )
+   if (( ${#files[@]} ));
+   then
+      rm -v *.jpg
+      echo "Done."
+   else
+      echo "Nothing to do."
+   fi
+   exit 0
+}
+
+if [ "$#" == 1 ]
+then
+   if [[ $1 =~ ^-?[0-9]+$ ]]
+   then
+      loop=$1
+   elif [[ $1 =~ "clean" ]]
+   then
+      clean
+   fi
+fi
+
+# exit
 
 # $imgsnap $dropbox/tmp/`date +%d%m%y%H%M%S`_snap.jpg
 
@@ -31,6 +60,15 @@ while true; do
    echo "Done."
 
    echo
+   
+   if [ -n "$loop" ]
+   then
+      loop=$(($loop - 1))
+      if [ $loop -lt 1 ]
+      then
+         exit 0
+      fi
+   fi
 
    sleep $interval
 
